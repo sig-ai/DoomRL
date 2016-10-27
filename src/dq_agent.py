57888778ef102e6306c5afbcaadc3caf90eac27e
Akshay Mishra <lck.akshay@gmail.com>
2016-10-26 19:15:31 -0500

    Cleaned up code

--- src/dq_agent.py
+++ src/dq_agent.py
@@ -3,38 +3,6 @@ import tensorflow as tf
 from tensorflow import nn
 from tensorflow.contrib import losses, slim
 
-obs_shape = [120, 160, 3]
-num_actions = 8
-
-import numpy as np
-from random import randint
-
-
-def random_obs(n):
-    return np.random.random([n] + obs_shape)
-
-
-def random_batch(n):
-    ob = random_obs(n)
-    ob_next = random_obs(n)
-    r = np.random.random(n)
-    a = np.array([randint(0, 8) for _ in range(n)])
-    return ob, ob_next, a, r
-
-
-def make_network(obs_shape, num_actions, sess,
-                 num_filters=[16, 32, 32, 64, 64],
-                 num_units=[128, 128]):
-    """
-    Constructs a q network with num_filters convolutions for
-    initial layers, followed by hidden layers with units specified
-    in num_units.
-
-    obs and q_targets are placeholders,
-    and q_values are the network outputs for a given state.
-    """
-    return obs, q_targets, q_values, train_step, loss
-
 
 class DQAgent(object):
 
@@ -90,24 +58,3 @@ class DQAgent(object):
     def act(self, obs):
         q_values = Q(self, [obs])
         return np.argmax(q_values)
-
-
-def Q(agent, obs):
-    q_values = agent.q_values.eval({agent.obs: obs}, agent.sess)
-    return q_values
-
-
-def learn(agent, batch):
-    batch_size = len(batch[0])
-    ob, ob_next, a, r = batch
-    q_values = Q(agent, ob)
-    q_targets = q_values.copy()
-    q_targets[np.arange(batch_size), a] = r + agent.discount_factor * \
-        np.max(Q(agent, ob_next), 1)
-    agent.sess.run([agent.loss, agent.train_step], {
-        agent.obs: ob, agent.q_targets: q_targets})
-
-
-def act(agent, obs):
-    q_values = Q(agent, [obs])
-    return np.argmax(q_values)
