ee8ac71cef85310ba8074a59b9e3b76fb1c93f93
Akshay Mishra <lck.akshay@gmail.com>
2016-10-26 23:43:02 -0500

    updated baseline's parameters

--- src/baseline.py
+++ src/baseline.py
@@ -6,7 +6,7 @@ from dq_agent import DQAgent
 def main():
     env = basic_env()
     agent = DQAgent(env)
-    learn_doom(agent, env, episodes=10, replay_buffer_size=3)
+    learn_doom(agent, env, episodes=100000, replay_buffer_size=1000, batch_size=64)
     
 
 if __name__=='__main__':
