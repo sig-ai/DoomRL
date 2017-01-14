c4e7ef967be3410eaace4af1918b8f117240089e
Akshay Mishra <lck.akshay@gmail.com>
2017-01-13 22:47:45 -0500

    being terrible cuz my gym_pull broke

--- src/main.py
+++ src/main.py
@@ -4,7 +4,26 @@ from dq_agent import DQAgent
 import gym
 import tensorflow as tf
 from gym.wrappers import SkipWrapper
-from components.common import decay_fn
+#from components.common import decay_fn
+
+def decay_fn(total_iterations, output_range=[1,.1]):
+    """
+    Returns a function that decays its outputs with respect
+    to the iterations. Useful for decreasing exploration and
+    learning rates over time.
+    """
+    first_output, last_output = output_range
+    step_update = (last_output - first_output) / total_iterations
+
+    def decay(step):
+        """ Output decays linearly with iterations. """
+        if step > total_iterations:
+            return last_output
+        return first_output + step * step_update
+        
+    return decay
+
+
 
 tf.python.control_flow_ops = tf
 names=['ppaquette/DoomBasic-v0',            #0
