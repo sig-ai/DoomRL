3195285657f5ca1d7da775ac95b73fb758a456d9
Akshay Mishra <lck.akshay@gmail.com>
2017-01-13 22:31:18 -0500

    added default decay range

--- src/components/common.py
+++ src/components/common.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 
-def decay_fn(total_iterations, output_range):
+def decay_fn(total_iterations, output_range=[1,.1]):
     """
     Returns a function that decays its outputs with respect
     to the iterations. Useful for decreasing exploration and
